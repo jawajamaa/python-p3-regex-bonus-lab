260c680844d1cd6ceace5c5a46ea7c29d23f3fd4
Timothy Ryon <tim@timryon.com>
2024-07-12 12:32:49 -0400

    finish and pass all 4 tests

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,5 @@
 import re
 
-my_pattern = r""
+my_pattern = r"It's such a lovely day today.|Some weather we're having today, huh\?|Maybe today's just not my day."
 my_regex = re.compile(my_pattern)
-
+# Is there a actual regex pattern (instead of 3 different literals) that would match these, and more sentences?  Chat GPT doesn't think so and returned my solution to me as its own solution!
